54a6187d7af128b092d4610af9aef58db1dcc6fe
Astemir Eleev <astemireleev@gmail.com>
2018-10-24 16:44:59 +0300

    Created WorldSize

---
+++ snake Shared/Core/WorldSize.swift
@@ -0,0 +1,11 @@
+//
+//  WorldSize.swift
+//  snake
+//
+//  Created by Astemir Eleev on 24/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+typealias WorldSize = Int
