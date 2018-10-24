05533362b50fe3287a47058b2e0da71aabc1de1b
Astemir Eleev <astemireleev@gmail.com>
2018-10-24 16:11:11 +0300

    Initial Commit

---
+++ snake macOS/GameViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GameViewController.swift
+//  snake macOS
+//
+//  Created by Astemir Eleev on 24/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
