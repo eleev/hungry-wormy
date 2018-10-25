5d41237cf492a3763c8877318faa97ab6a0ef3fb
Astemir Eleev <astemireleev@gmail.com>
2018-10-25 10:35:50 +0300

    Created TileLevel class

---
+++ snake Shared/TileLevel.swift
@@ -0,0 +1,14 @@
+//
+//  TileLevel.swift
+//  snake
+//
+//  Created by Astemir Eleev on 25/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import SpriteKit
+
+class TileLevel {
+    
+    
+}
