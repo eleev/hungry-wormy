541e3a412b8b74cf7df57dd81c9db6ebb1f2013b
Astemir Eleev <astemireleev@gmail.com>
2018-10-26 15:49:49 +0300

    Implemented SnakeIncreaseLevel enum type

---
+++ snake Shared/Snake/Nodes & Factories/SnakeIncreaseLevel.swift
@@ -0,0 +1,13 @@
+//
+//  SnakeIncreaseLevel.swift
+//  snake
+//
+//  Created by Astemir Eleev on 26/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+enum SnakeIncreaseLevel: Int {
+    case one = 1, two, three
+}
