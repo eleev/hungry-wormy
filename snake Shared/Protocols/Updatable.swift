4efe935ed655e3ed2af05d9802b90a1fd29c3479
Astemir Eleev <astemireleev@gmail.com>
2018-10-26 15:44:15 +0300

    Implemented Updatable protocol

---
+++ snake Shared/Protocols/Updatable.swift
@@ -0,0 +1,13 @@
+//
+//  Updatable.swift
+//  snake
+//
+//  Created by Astemir Eleev on 26/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+protocol Updatable {
+    func update()
+}
