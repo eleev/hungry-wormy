7fead886b32be40240fbf47329d6821f7942a241
Astemir Eleev <astemireleev@gmail.com>
2018-10-28 11:32:15 +0300

    Implemented animation

--- snake Shared/Fruits/FruitNode.swift
+++ snake Shared/Fruits/FruitNode.swift
@@ -15,13 +15,23 @@ class FruitNode: SKSpriteNode {
         
         self.position = position
         self.zPosition = zPosition
-        physicsBody = SKPhysicsBody(rectangleOf: texture.size())
+        physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
         physicsBody?.isDynamic = false
         physicsBody?.isResting = true
+        
+        animate()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func animate() {
+        let scaleUp = SKAction.scale(to: 1.0, duration: 1.0)
+        let scaleDown = SKAction.scale(to: 0.8, duration: 1.0)
+        let sequence = SKAction.sequence([scaleUp, scaleDown])
+        let foreverSequence = SKAction.repeatForever(sequence)
+        run(foreverSequence)
     }
     
     func remove() {
