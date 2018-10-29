9467ded04b6599c60731de49515153c352ec60db
Astemir Eleev <astemireleev@gmail.com>
2018-10-29 20:24:08 +0300

    Implemented Direction extensions

--- snake Shared/Snake/Model/Direction.swift
+++ snake Shared/Snake/Model/Direction.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 enum Direction {
-    case left, right, up, down
+    case left, right, up, down, none
 }
 
 extension Direction {
@@ -22,7 +22,44 @@ extension Direction {
             isSwitched = direction == .up || direction == .down
         case .up, .down:
             isSwitched = direction == .left || direction == .right
+        case .none:
+            return false
         }
         return isSwitched
     }
+}
+
+extension Direction {
+    
+    func toRotationDegrees() -> CGFloat {
+        switch self {
+        case .up:
+            return 0
+        case .down:
+            return 180
+        case .left:
+            return 90
+        case .right:
+            return -90
+        case .none:
+            return -1
+        }
+    }
+    
+    func reversed() -> Direction {
+        switch self {
+        case .up:
+            return .down
+        case .down:
+            return .up
+        case .left:
+            return .right
+        case .right:
+            return .left
+        case .none:
+            return self
+        }
+    }
+    
+    
 }
