226d98985a5b55c51456ca326841b5ee6f00051a
Astemir Eleev <astemireleev@gmail.com>
2018-10-29 20:23:58 +0300

    Implemented custom SKSpriteNode classes

--- snake Shared/Snake/Nodes & Factories/SnakePartNode.swift
+++ snake Shared/Snake/Nodes & Factories/SnakePartNode.swift
@@ -11,3 +11,27 @@ import SpriteKit
 class SnakePartNode: SKSpriteNode {
     // Has no implementation
 }
+
+class SnakeHeadNode: SnakePartNode, Updatable {
+    
+    var direction: Direction = .up
+    
+    func update() {
+        zRotation = direction.toRotationDegrees().toRadians
+    }
+}
+
+class SnakeTailNode: SnakePartNode, Updatable {
+    
+    var direction: Direction = .up
+ 
+    func update() {
+        zRotation = direction.reversed().toRotationDegrees().toRadians
+    }
+}
+
+
+extension FloatingPoint {
+    var toRadians: Self { return self * .pi / 180 }
+    var toDegrees: Self { return self * 180 / .pi }
+}
