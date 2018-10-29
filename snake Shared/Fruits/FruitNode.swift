decbcb9094077d71894362ce87b8e011d068d6d6
Astemir Eleev <astemireleev@gmail.com>
2018-10-29 20:20:03 +0300

    Implemented FoodNode

--- snake Shared/Fruits/FruitNode.swift
+++ snake Shared/Fruits/FruitNode.swift
@@ -15,7 +15,7 @@ class FruitNode: SKSpriteNode {
         
         self.position = position
         self.zPosition = zPosition
-        physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
+        physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 54, height: 54))
         physicsBody?.isDynamic = false
         physicsBody?.isResting = true
         
@@ -39,5 +39,67 @@ class FruitNode: SKSpriteNode {
         removeAllActions()
         removeFromParent()
 
+    }
+}
+
+class FoodNode: SKSpriteNode {
+    
+    init(position: CGPoint, zPosition: CGFloat, type: FoodType) {
+        let textures = type.textures()
+        super.init(texture: textures[0], color: .clear, size: CGSize(width: 64, height: 64))
+        
+        self.position = position
+        self.zPosition = zPosition
+        
+        physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 54, height: 54))
+        physicsBody?.isDynamic = false
+        physicsBody?.isResting = true
+        
+        animate()
+        
+        let textureAnimation = SKAction.animate(with: textures, timePerFrame: 0.2)
+        let foreverAction = SKAction.repeatForever(textureAnimation)
+        run(foreverAction)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func animate() {
+        let scaleUp = SKAction.scale(to: 1.0, duration: 1.0)
+        let scaleDown = SKAction.scale(to: 0.8, duration: 1.0)
+        let sequence = SKAction.sequence([scaleUp, scaleDown])
+        let foreverSequence = SKAction.repeatForever(sequence)
+        run(foreverSequence)
+    }
+    
+    func remove() {
+        // Animate the removal
+        removeAllActions()
+        removeFromParent()
+        
+    }
+    
+}
+
+enum FoodType {
+    case bee
+    case ladybug
+
+    enum Fish {
+        case green
+        case pink
+    }
+}
+
+extension FoodType {
+    func textures() -> [SKTexture] {
+        switch self {
+        case .bee:
+            return [SKTexture(imageNamed: "bee"), SKTexture(imageNamed: "bee_move")]
+        case .ladybug:
+            return [SKTexture(imageNamed: "ladybug"), SKTexture(imageNamed: "ladybug_move")]
+        }
     }
 }
