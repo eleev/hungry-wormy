8e7616016eea8d86125f929b667aefbb1c3b33bd
Astemir Eleev <astemireleev@gmail.com>
2018-10-29 20:21:15 +0300

    implemented new producers

--- snake Shared/Snake/Nodes & Factories/SnakeNodeFactory.swift
+++ snake Shared/Snake/Nodes & Factories/SnakeNodeFactory.swift
@@ -31,15 +31,59 @@ struct SnakeNodeFactory {
         return produce(from: randomSnakePart)
     }
     
+    func produceHead() -> SnakeHeadNode {
+        let headClosedTexture = SKTexture(imageNamed: "head-closed")
+        let node = SnakeHeadNode(texture: headClosedTexture, size: nodeSize)
+        
+        let animation = SKAction.animate(with: [headClosedTexture, SKTexture(imageNamed: "head-open")], timePerFrame: 0.25)
+        let foreverAnimation = SKAction.repeatForever(animation)
+        node.run(foreverAnimation)
+        
+        let physicsBody = SKPhysicsBody(rectangleOf: nodeSize)
+        physicsBody.isDynamic = true
+        physicsBody.affectedByGravity = false
+        physicsBody.contactTestBitMask = PhysicsTypes.snake.rawValue | PhysicsTypes.wall.rawValue | PhysicsTypes.fruit.rawValue
+        physicsBody.categoryBitMask = PhysicsTypes.snake.rawValue
+        physicsBody.collisionBitMask = 0
+        
+        node.physicsBody = physicsBody
+        return node
+    }
+    
+    func produceBody() -> SnakePartNode {
+        let headClosedTexture = SKTexture(imageNamed: "body")
+        let node = SnakePartNode(texture: headClosedTexture, size: nodeSize)
+        
+        let physicsBody = SKPhysicsBody(rectangleOf: nodeSize)
+        physicsBody.isDynamic = true
+        physicsBody.affectedByGravity = false
+        physicsBody.contactTestBitMask = PhysicsTypes.snake.rawValue | PhysicsTypes.wall.rawValue | PhysicsTypes.fruit.rawValue
+        physicsBody.categoryBitMask = PhysicsTypes.snake.rawValue
+        physicsBody.collisionBitMask = 0
+        
+        node.physicsBody = physicsBody
+        return node
+    }
+    
+    
     func produce(from part: SnakeParts) -> SnakePartNode {
         let texture = part.getTexture()
         let node = SnakePartNode(texture: texture, size: nodeSize)
         
         let physicsBody = SKPhysicsBody(rectangleOf: nodeSize)
-        physicsBody.isDynamic = false
+        physicsBody.isDynamic = true
         physicsBody.affectedByGravity = false
+        physicsBody.contactTestBitMask = PhysicsTypes.snake.rawValue | PhysicsTypes.wall.rawValue | PhysicsTypes.fruit.rawValue
+        physicsBody.categoryBitMask = PhysicsTypes.snake.rawValue
+        physicsBody.collisionBitMask = 0
         
         node.physicsBody = physicsBody
         return node
     }
+}
+
+enum PhysicsTypes: UInt32 {
+    case snake = 1
+    case wall = 2
+    case fruit = 4
 }
