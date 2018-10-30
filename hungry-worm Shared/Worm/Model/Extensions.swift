0174aee5ba6642767cf5aacbbc6a5183e3eeba5b
Astemir Eleev <astemireleev@gmail.com>
2018-10-30 16:33:53 +0300

    Created new CGPoint extension with custom operators

---
+++ hungry-worm Shared/Worm/Model/Extensions.swift
@@ -0,0 +1,39 @@
+//
+//  Extensions.swift
+//  snake
+//
+//  Created by Astemir Eleev on 24/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import CoreGraphics
+
+extension Int {
+    
+    var toCGFloat: CGFloat {
+        return CGFloat(self)
+    }
+}
+
+extension CGFloat {
+    
+    var toInt: Int {
+        return Int(self)
+    }
+}
+
+infix operator -%
+
+extension CGPoint {
+    static func -(lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        return CGPoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+    }
+    
+    static func +(lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        return CGPoint(x: lhs.x + rhs.x, y: lhs.y + lhs.y)
+    }
+
+    static func -%(lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        return CGPoint(x: abs(lhs.x - rhs.x), y: abs(lhs.y - rhs.y))
+    }
+}
