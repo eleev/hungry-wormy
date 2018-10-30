69ea227431cecdc25993a600e355aec017189a4b
Astemir Eleev <astemireleev@gmail.com>
2018-10-30 16:34:02 +0300

    Created Direction extension

---
+++ hungry-worm Shared/Worm/Model/Direction.swift
@@ -0,0 +1,65 @@
+//
+//  Direction.swift
+//  snake
+//
+//  Created by Astemir Eleev on 24/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import CoreGraphics
+
+enum Direction {
+    case left, right, up, down, none
+}
+
+extension Direction {
+    
+    func isAbleToSwitch(toNew direction: Direction) -> Bool {
+        var isSwitched = false
+        
+        switch self {
+        case .left, .right:
+            isSwitched = direction == .up || direction == .down
+        case .up, .down:
+            isSwitched = direction == .left || direction == .right
+        case .none:
+            return false
+        }
+        return isSwitched
+    }
+}
+
+extension Direction {
+    
+    func toRotationDegrees() -> CGFloat {
+        switch self {
+        case .up:
+            return 0
+        case .down:
+            return 180
+        case .left:
+            return 90
+        case .right:
+            return -90
+        case .none:
+            return -1
+        }
+    }
+    
+    func reversed() -> Direction {
+        switch self {
+        case .up:
+            return .down
+        case .down:
+            return .up
+        case .left:
+            return .right
+        case .right:
+            return .left
+        case .none:
+            return self
+        }
+    }
+    
+    
+}
