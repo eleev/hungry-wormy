744c72fed5b1a7d9845ce9c1562f63d2fc3da047
Astemir Eleev <astemireleev@gmail.com>
2018-10-30 16:33:24 +0300

    Created WormPartNode class

---
+++ hungry-worm Shared/Worm/Nodes/WormPartNode.swift
@@ -0,0 +1,45 @@
+//
+//  WormPartNode.swift
+//  snake
+//
+//  Created by Astemir Eleev on 26/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import SpriteKit
+
+class WormPartNode: SKSpriteNode {
+    // Has no implementation
+}
+
+class WormHeadNode: WormPartNode, Updatable {
+    
+    // MARK: - Properties
+    
+    var direction: Direction = .up
+    
+    // MARK: - Methods
+    
+    func update() {
+        zRotation = direction.toRotationDegrees().toRadians
+    }
+}
+
+class SnakeTailNode: WormPartNode, Updatable {
+    
+    // MARK: - Properties
+    
+    var direction: Direction = .up
+    
+    // MARK: - Methods
+    
+    func update() {
+        zRotation = direction.reversed().toRotationDegrees().toRadians
+    }
+}
+
+
+extension FloatingPoint {
+    var toRadians: Self { return self * .pi / 180 }
+    var toDegrees: Self { return self * 180 / .pi }
+}
