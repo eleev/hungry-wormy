12b9e413aca7815127e72b8290661aaa40204ea1
Astemir Eleev <astemireleev@gmail.com>
2018-10-30 16:32:16 +0300

    Created FruitGenerator class

---
+++ hungry-worm Shared/Fruits/FruitGenerator.swift
@@ -0,0 +1,74 @@
+//
+//  FruitGenerator.swift
+//  snake
+//
+//  Created by Astemir Eleev on 26/10/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import SpriteKit
+
+struct FruitGenerator {
+    
+    // MARK: - Properties
+    
+    private var spawnPoints: [CGPoint]
+    private let zPosition: CGFloat
+    private var lastFruit: FoodNode?
+    
+    // MARK: - Initializers
+    
+    init(spawnPoints: [CGPoint], zPosition: CGFloat) {
+        self.spawnPoints = spawnPoints
+        self.zPosition = zPosition
+    }
+    
+    // MARK: - Methods
+    
+    mutating func generate() -> FoodNode {
+//        let randomItem = Item.random() ?? Item.red
+//        let texture = randomItem.get()
+//
+        let index = Int.random(in: 0..<spawnPoints.count)
+        let point = spawnPoints.remove(at: index)
+        
+        let node = FoodNode(position: point, zPosition: zPosition, type: .bee)
+        lastFruit = node
+        return node
+    }
+    
+    func removeLastFruit() {
+        lastFruit?.remove()
+    }
+    
+    func isDrained() -> Bool {
+        return spawnPoints.isEmpty
+    }
+}
+
+extension FruitGenerator: PhysicsContactDelegate {
+    
+    func didBeginPhysicsContact(_ contact: SKPhysicsContact, completion:  @escaping (Bool) -> ()) {
+        
+        func removeAndComplete(_ fruit: FoodNode) {
+            fruit.remove()
+            completion(true)
+        }
+        
+        // WormPart + FruitNode intersection
+        if contact.bodyA.node is WormPartNode, let fruit = contact.bodyB.node as? FoodNode  {
+            // Remove fruit and grow the worm
+            removeAndComplete(fruit)
+        } else if contact.bodyB.node is WormPartNode, let fruit =  contact.bodyA.node as? FoodNode {
+            // Remove fruit and grow the worm
+            removeAndComplete(fruit)
+        } else {
+            completion(false)
+        }
+    }
+    
+    func didEndPhysicsContact(_ contact: SKPhysicsContact) {
+        // Has no implementation
+    }
+    
+}
