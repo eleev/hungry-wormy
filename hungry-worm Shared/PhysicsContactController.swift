8b9bdbf518a76081d60511318d31bd3da1ecbd39
Astemir Eleev <astemireleev@gmail.com>
2018-11-02 19:53:14 +0300

    Implemented worm collision handling with walls

--- hungry-worm Shared/PhysicsContactController.swift
+++ hungry-worm Shared/PhysicsContactController.swift
@@ -12,10 +12,10 @@ class PhysicsContactController: PhysicsContactDelegate {
     
     // MARK: - Properties
     
-    private var worm: WormNode
+    private weak var worm: WormNode?
+    private weak var scene: SKScene?
     private var deathHandler: ()->()
-    private var fruitGenerator: FruitGenerator
-    private var scene: SKScene
+    private var fruitGenerator: FruitGenerator?
     
     // MARK: - Initializers
     
@@ -27,28 +27,49 @@ class PhysicsContactController: PhysicsContactDelegate {
     }
     
     func generateFruit() {
-        fruitGenerator.removeLastFruit()
-        let fruitNode = fruitGenerator.generate()
-        scene.addChild(fruitNode)
+        fruitGenerator?.removeLastFruit()
+        let fruitNode = fruitGenerator?.generate()
+        scene?.addChild(fruitNode!)
     }
     
     // MARK: - Conformance to PhysocsContactDelegate protocol
     
-    func didBeginPhysicsContact(_ contact: SKPhysicsContact, completion: @escaping (Bool) -> ()) {
+    func didBeginPhysicsContact(_ contact: SKPhysicsContact, completion: @escaping (Bool) -> () = { _ in }) {
         
-        let nodeA = contact.bodyA.node
-        let nodeB = contact.bodyB.node
+        let bodyA = contact.bodyA
+        let bodyB = contact.bodyB
+        let nodeA = bodyA.node
+        let nodeB = bodyB.node
         
-        if nodeA is FoodNode, nodeB is WormNode || nodeA is WormNode, nodeB is FoodNode{
-            generateFruit()
-            worm.grow()
-        }
-        
-        #warning("Handle snake death when hitting a wall")
+        resolveWormGrowing(for: nodeA, and: nodeB)
+        resolveWormWallCollision(for: bodyA, and: bodyB)
     }
     
     func didEndPhysicsContact(_ contact: SKPhysicsContact) {
         // Has not been implemented
     }
 
+    
+    // MARK: - Private methods
+    
+    private func resolveWormGrowing(for nodeA: SKNode?, and nodeB: SKNode?) {
+        func growWorm() {
+            generateFruit()
+            worm?.grow()
+        }
+        
+        if nodeA is FoodNode, nodeB is WormPartNode {
+            growWorm()
+        } else if nodeA is WormPartNode, nodeB is FoodNode {
+            growWorm()
+        }
+    }
+    
+    private func resolveWormWallCollision(for bodyA: SKPhysicsBody, and bodyB: SKPhysicsBody) {
+        if bodyA.categoryBitMask == PhysicsTypes.wall.rawValue, bodyB.categoryBitMask == PhysicsTypes.snake.rawValue {
+            deathHandler()
+        } else if bodyA.categoryBitMask == PhysicsTypes.snake.rawValue, bodyB.categoryBitMask == PhysicsTypes.wall.rawValue {
+            deathHandler()
+        }
+    }
 }
