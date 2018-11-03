9ec67b68393dea5871e8db8825ef0bfa1cd40fb9
Astemir Eleev <astemireleev@gmail.com>
2018-11-03 14:03:32 +0300

    Create PhysicsTypes enum type

---
+++ hungry-worm Shared/Worm/Model/PhysicsTypes.swift
@@ -0,0 +1,15 @@
+//
+//  PhysicsTypes.swift
+//  hungry-worm
+//
+//  Created by Astemir Eleev on 03/11/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+enum PhysicsTypes: UInt32 {
+    case snake = 1
+    case wall = 2
+    case fruit = 4
+}
