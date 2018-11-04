4259bbb33cec1c3f5a82bf54799d840b455c11e3
Astemir Eleev <astemireleev@gmail.com>
2018-11-04 20:56:59 +0300

    Refactored controller by replacing the game scene to main menu scene

--- hungry-worm macOS/GameViewController.swift
+++ hungry-worm macOS/GameViewController.swift
@@ -15,7 +15,11 @@ class GameViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let scene = GameScene.newGameScene()
+        guard let scene = SKScene(fileNamed: "MainMenuScene") as? MainMenuScene else {
+            print("Failed to load MainMenuScene.sks")
+            abort()
+        }
+        scene.scaleMode = .aspectFit
         
         // Present the scene
         let skView = self.view as! SKView
