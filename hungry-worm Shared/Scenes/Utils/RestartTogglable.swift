a70b7782538889d8bb506ffc2540853b23ed91c1
Astemir Eleev <astemireleev@gmail.com>
2018-12-06 18:46:21 +0300

    Created RestartTogglable protocol

---
+++ hungry-worm Shared/Scenes/Utils/RestartTogglable.swift
@@ -0,0 +1,13 @@
+//
+//  RestartTogglable.swift
+//  hungry-worm iOS
+//
+//  Created by Astemir Eleev on 06/12/2018.
+//  Copyright © 2018 Astemir Eleev. All rights reserved.
+//
+
+import Foundation
+
+protocol RestartTogglable: class {
+    func didRequestRestart() 
+}
